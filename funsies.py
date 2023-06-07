2a5c5661b3ac475620195c04c2a070528160e924
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-07 12:38:40 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('Welcome to Dublin!')
 
-st.markdown("![Alt Text](<iframe src="https://giphy.com/embed/xT9IgzoKnwFNmISR8I" width="480" height="429" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/code-coding-seamless-xT9IgzoKnwFNmISR8I">via GIPHY</a></p>)")
+st.markdown("![Alt Text]("https://giphy.com/embed/xT9IgzoKnwFNmISR8I")
