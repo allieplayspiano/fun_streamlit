534af8ba9d61c224e451bd4173a3b5c1bc8f07f7
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-07 12:40:19 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('Welcome to Dublin!')
 
-st.markdown("![Alt Text](https://giphy.com/embed/xT9IgzoKnwFNmISR8I")
+st.markdown("![Alt Text](https://giphy.com/embed/xT9IgzoKnwFNmISR8I)
