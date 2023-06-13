e88601c0c2ea6784a35a9943d3c3b2447f7c33aa
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 15:31:02 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -1,6 +1,34 @@
 import streamlit as st
+import pandas as pd
 from PIL import Image
 
-image = Image.open('sunrise.jpg')
+# Set page title and favicon
+st.set_page_config(page_title='Dublin Bay Area Guide', page_icon=':mountain:')
 
-st.image(image, caption='Sunrise by the mountains')
+# Load cafe, restaurant, and hike data
+cafes_data = pd.read_csv('cafes.csv')  # Replace 'cafes.csv' with the actual path to your cafes data file
+restaurants_data = pd.read_csv('restaurants.csv')  # Replace 'restaurants.csv' with the actual path to your restaurants data file
+hikes_data = pd.read_csv('hikes.csv')  # Replace 'hikes.csv' with the actual path to your hikes data file
+
+# Load images
+cafe_image = Image.open('cafe_image.jpg')  # Replace 'cafe_image.jpg' with the actual path to your cafe image
+restaurant_image = Image.open('restaurant_image.jpg')  # Replace 'restaurant_image.jpg' with the actual path to your restaurant image
+hike_image = Image.open('hike_image.jpg')  # Replace 'hike_image.jpg' with the actual path to your hike image
+
+# Create sidebar selection
+selected_category = st.sidebar.selectbox('Select Category', ('Cafes', 'Restaurants', 'Hikes'))
+
+if selected_category == 'Cafes':
+    st.image(cafe_image, use_column_width=True)
+    st.header('Cafes in Dublin Bay Area')
+    st.dataframe(cafes_data)
+
+elif selected_category == 'Restaurants':
+    st.image(restaurant_image, use_column_width=True)
+    st.header('Restaurants in Dublin Bay Area')
+    st.dataframe(restaurants_data)
+
+elif selected_category == 'Hikes':
+    st.image(hike_image, use_column_width=True)
+    st.header('Hikes in Dublin Bay Area')
+    st.dataframe(hikes_data)
