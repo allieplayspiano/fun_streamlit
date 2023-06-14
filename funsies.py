5f9dd386ac5630eba1f19ac487a4756236cbff46
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:49:25 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 st.set_page_config(page_title="Welcome to the Dublin Office!", page_icon=":tada:", layout="wide")
+st.title("Welcome to the Dublin Office! :tada:")
 st.subheader("Hi, I'm Allison!")
