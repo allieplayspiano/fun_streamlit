6fae3d0a64e2e0ffbc257bcf4cb7b35d56bbbae4
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:23:42 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -6,4 +6,4 @@ from PIL import Image
 st.set_page_config(page_title='Dublin Bay Area Guide', page_icon=':mountain:')
 
 # Load cafe, restaurant, and hike data
-cafes_data = pd.read_csv('/Users/alfernandez/Documents/cafes.csv')  # Replace 'cafes.csv' with the actual path to your cafes data file
+cafes_data = pd.read_csv(r'/Users/alfernandez/Documents/cafes.csv')  # Replace 'cafes.csv' with the actual path to your cafes data file
