4cb856d433917c1d9eef92afb64ef6f010782226
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:47:31 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -1,6 +1,3 @@
 import streamlit as st
-import pandas as pd
-from PIL import Image
 
-st.markdown('_Allison has been on a journey of learning to code._')
 st.set_page_config(page_title="Welcome to the Dublin Office!", page_icon=":tada:", layout="wide")
