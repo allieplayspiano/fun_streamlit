fe978205bbd60b31c740d4689855bb13e74e939d
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:29:50 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-pip install folium
 
 # Set page title and favicon
 st.set_page_config(page_title='Dublin Bay Area Guide', page_icon=':mountain:')
