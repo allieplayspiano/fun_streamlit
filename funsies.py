9ea5c7d3f6221a5bfc54e3599da86b4e317e905d
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:37:07 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -2,5 +2,6 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-st.header('Welcome to the Dublin Office!')
+st.title('Welcome to the Dublin Office!')
 st.markdown('_Allison has been on a journey of learning to code._')
+
