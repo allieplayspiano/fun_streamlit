1d1760f56438644ad913dad98abf6da9640a3903
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:29:27 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+pip install folium
 
 # Set page title and favicon
 st.set_page_config(page_title='Dublin Bay Area Guide', page_icon=':mountain:')
 
-# Load cafe, restaurant, and hike data
-cafes_data = pd.read_csv(r'/Users/alfernandez/Documents/cafes.csv')  # Replace 'cafes.csv' with the actual path to your cafes data file
+import folium
+
+# Create a map centered at Dublin, California
+map_dublin = folium.Map(location=[37.7022, -121.9358], zoom_start=12)
+
+# Add a marker at Dublin, California
+folium.Marker(location=[37.7022, -121.9358], popup="Dublin, California").add_to(map_dublin)
+
+# Display the map
+map_dublin.save("dublin_map.html")
