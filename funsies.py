55bd305c15a88960e90683918131be17e9cb4777
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:46:38 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -2,7 +2,5 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-st.title('Welcome to the Dublin Office!')
 st.markdown('_Allison has been on a journey of learning to code._')
-from streamlit_folium import st_folium
-import folium
+st.set_page_config(page_title="Welcome to the Dublin Office!", page_icon=":tada:", layout="wide")
