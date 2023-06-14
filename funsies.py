7cf60bc0e985fbe35157282724f5f467feb95cb5
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:31:55 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -4,15 +4,7 @@ from PIL import Image
 
 # Set page title and favicon
 st.set_page_config(page_title='Dublin Bay Area Guide', page_icon=':mountain:')
-$ pip install folium
 
-import folium
+image = Image.open('sunrise.jpg')
 
-# Create a map centered at Dublin, California
-map_dublin = folium.Map(location=[37.7022, -121.9358], zoom_start=12)
-
-# Add a marker at Dublin, California
-folium.Marker(location=[37.7022, -121.9358], popup="Dublin, California").add_to(map_dublin)
-
-# Display the map
-map_dublin.save("dublin_map.html")
+st.image(image, caption='Sunrise by the mountains')
