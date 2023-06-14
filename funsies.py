371b7908d44cbda8c8cfee3f0a62dd62fc82b4e4
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:34:33 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -2,5 +2,4 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-# Set page title and favicon
-st.set_page_config(page_title='Dublin Bay Area Guide', page_icon=':mountain:')
+st.header('Welcome to the Dublin Office!')
