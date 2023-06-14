870cfe5b0993df9509a758c32f9c6450d9ccfb17
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:52:08 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -4,3 +4,4 @@ st.set_page_config(page_title="Welcome to the Dublin Office!", page_icon=":tada:
 st.title("Welcome to the Dublin Office! :tada:")
 st.header("Hi, I'm Allison!")
 st.subheader("We are so excited to have you here :wave:")
+st.write("I'm your friendly BSA from the GTM-Applications team. Come swing by anytime if you have a question!")
