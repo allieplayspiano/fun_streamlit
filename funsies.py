0ca78af2f89980c38fc4170b938aa39562262e6e
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:39:36 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -4,4 +4,5 @@ from PIL import Image
 
 st.title('Welcome to the Dublin Office!')
 st.markdown('_Allison has been on a journey of learning to code._')
-
+from streamlit_folium import st_folium
+import folium
