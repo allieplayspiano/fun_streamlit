ace47f03faa8039e7555188adc7ab67063395741
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-13 18:56:49 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -6,3 +6,13 @@ st.header("Hi, I'm Allison!")
 st.subheader("We are so excited to have you here :wave:")
 st.write("I'm your friendly BSA from the GTM-Applications team. Come swing by anytime if you have a question!")
 st.write("[Learn More about Snowflake here>](https://www.snowflake.com/en/)")
+
+with st.container():
+  st.write("---")
+  left_column, right_column = st.columns(2)
+  with left_column:
+    st.header("What I've been working on")
+    st.write(##)
+      st. write(
+        """Status reporting, Billable Events, EDU Case Management, and more""")
+  
