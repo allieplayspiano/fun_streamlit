2adecffa1f791557232bb55c589481680c555f62
allieplayspiano <88855491+allieplayspiano@users.noreply.github.com>
2023-06-15 11:09:13 -0700

    Update funsies.py

--- funsies.py
+++ funsies.py
@@ -15,8 +15,8 @@ with st.container():
     Completed: Workday <> Financial Force PTO Integration
     - Automating the creation of PTO assignments within Financial Force in order free up to 25% of time per week for Resource Delivery Managers. (Current process takes 5-10 hours per RDM). 
    
-    Phase I of II Complete: Rebillable Expenses Automation 
-    ##Go-Live: July 13, 2023
+    Phase I of II Complete: Rebillable Expenses Automation \n
+    Go-Live: July 13, 2023
     - Automatically syncing expenses submitted/approved in Workday for PS Engagements into Financial Force for billing, saving up to 8 hours a month for the PS Operations Team 
 
     """)
